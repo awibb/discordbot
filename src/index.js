ebafd3637c9a1936377d28c9f4ca8ac1b920770a
andrew <andrew_wibbenmeyer@outlook.com>
2023-07-24 22:32:32 -0500

    prettier fixed

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-require('dotenv').config();
-const {Client, IntentsBitField} = require('discord.js');
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-    intents:[
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.on('ready', (c) => {
-
-    console.log(`${c.user.tag} is online`);
-
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online`);
 });
-
 
 client.on("messageCreate", (message) => {
-// console.log(message.content);
+  // console.log(message.content);
 
-if (message.author.bot) return;
+  if (message.author.bot) return;
 
-if (message.content == "hello"){
-    message.reply("Howdy Partner!")
-}
+  if (message.content == "hello") {
+    message.reply("Howdy Partner!");
+  }
 });
 
-
 client.login(process.env.TOKEN);
-
-
-
